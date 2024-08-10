6ffc691c404295cc38de991f76ddfb58ab8b8858
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-08-10 15:24:00 -0400

    added import for fs. added to writeToFile function.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
+import fs from "fs";
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -14,7 +15,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log(`Successfully generated README file in ${fileName}!`)
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
