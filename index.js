418844f5051e22f102ebaa19578de8c11dc8b580
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-08-10 15:17:23 -0400

    added import for inquirer. added question string elements for questions array

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //Asks for Project Title, Description, Installation Instructions, Usage Information, Contribution Guidelines,
+    //and Test Instructions
+    `What is the title of your project?`,
+    `What is the description of your project?`,
+    `What are the installation instructions for your project?`,
+    `What is the usage information for your project?`,
+    `What are the contribution guidelines for your project?`,
+    `What are the test instructions for your project?`,
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
